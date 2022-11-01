04dbb72501b883290049c9503f9ac6ee03902095
Tiara <tiaranurulh@gmail.com>
2022-11-01 11:21:08 +0700

    Initial Commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Tiara H on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
